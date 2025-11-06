54e8fb204955d7294a01b8ef78be5c6342fc3791
Tomoki Naito <tnaito@c6r3s11.42tokyo.jp>
2025-11-06 20:59:45 +0900

    hmm

---
+++ include/parser.h
@@ -0,0 +1,7 @@
+#ifndef PARSER_H
+# define PARSER_H
+
+int
+parser(t_lexer *token, t_parser **ast);
+
+#endif
--- src/parser.c
+++ src/parser.c
@@ -2,11 +2,30 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+static int
+parse_expr(t_lexer *token, t_parser **ast)
+{
+	return true;
+}
+
+static int
+parse_term(t_lexer *token, t_parser **ast)
+{
+	return true;
+}
+
+static int
+parse_factor(t_lexer *token, t_parser **ast)
+{
+	return true;
+}
+
 int
-parser(t_lexer *token, t_parser **parse)
+parser(t_lexer *token, t_parser **ast)
 {
 	if (!token) return false;
 	if (!parse) return false;
-	*parse = NULL;
-	return parser_set(token, parse);
+	*ast = NULL;
+	parse_factor(token, ast);
+	return (ast != NULL);
 }
